d62907d79e4bd051736c7c0ae3252db5e34c3a1c
“Chetankoli02” <chetankoli2016@gmail.com>
2023-07-08 09:29:03 +0530

    [Chetan]-Added number of times the dice was played

--- SnakeLadder/Program.cs
+++ SnakeLadder/Program.cs
@@ -1,41 +1,39 @@
 ﻿internal class Program
 {
     private static void Main(string[] args)
-    {  
-            int playerPosition = 0;
-            int diceRolls = 0;
-            Random random = new Random();
-            while (playerPosition != 100)
+    {
+        int playerPosition = 0;
+        int diceRolls = 0;
+        Random random = new Random();
+        while (playerPosition != 100)
+        {
+            int diceNumber = random.Next(1, 7);
+            diceRolls++;
+            Console.WriteLine("Dice rolled: " + diceNumber);
+            int option = random.Next(0, 3);
+            switch (option)
             {
-                int diceNumber = random.Next(1, 7);
-                diceRolls++;
-                Console.WriteLine("Dice rolled: " + diceNumber);
-                int option = random.Next(0, 3);
-                switch (option)
-                {
-                    case 0:
-                        Console.WriteLine("No Play. Stay in the same position.");
-                        break;
-                    case 1:
-                        Console.WriteLine("Ladder! Move ahead by " + diceNumber + " positions.");
-                        playerPosition += diceNumber;
-                        break;
-                    case 2:
-                        Console.WriteLine("Snake! Move back by " + diceNumber + " positions.");
-                        playerPosition -= diceNumber;
-                        break;
-                }
-                if (playerPosition < 0)
-                {
-                    playerPosition = 0;
-                }
-                if (playerPosition > 100)
-                {
+                case 0:
+                    Console.WriteLine("No Play. Stay in the same position.");
+                    break;
+                case 1:
+                    Console.WriteLine("Ladder! Move ahead by " + diceNumber + " positions.");
+                    Console.WriteLine("Moves ahead by " + diceNumber + " positions.");
+                    playerPosition += diceNumber;
+                    break;
+                case 2:
+                    Console.WriteLine("Snake! Move back by " + diceNumber + " positions.");
+                    Console.WriteLine("Moves behind by " + diceNumber + " positions.");
                     playerPosition -= diceNumber;
-                }
-
-                Console.WriteLine("Current position: " + playerPosition);
-                Console.WriteLine();
+                    break;
             }
+            Console.WriteLine("Current position: " + playerPosition);
+            Console.WriteLine();
+        }
+
+        Console.WriteLine(" You won the game.");
+        Console.WriteLine("Total dice rolls: " + diceRolls);
+        Console.ReadLine();
+
     }
 }
