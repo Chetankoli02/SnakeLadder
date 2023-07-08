6dc4ebcaae7e43a1369a6e367254574cc7928ba1
“Chetankoli02” <chetankoli2016@gmail.com>
2023-07-08 07:57:10 +0530

    [Chetan]-Added Number between 1 to 6]

--- SnakeLadder/Program.cs
+++ SnakeLadder/Program.cs
@@ -2,6 +2,12 @@
 {
     private static void Main(string[] args)
     {
-        int playerPosition = 0;
+
+        Random random = new Random();
+
+        // Roll the die and get a random number between 1 and 6
+        int diceNumber = random.Next(1, 7);
+
+        Console.WriteLine("The player rolled a " + diceNumber);
     }
 }
