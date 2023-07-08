ea7df1a288f24e171c27d152568f7003ff0e9a0e
“Chetankoli02” <chetankoli2016@gmail.com>
2023-07-08 08:22:31 +0530

    [Chetan]-Added exact winning position 100

--- SnakeLadder/Program.cs
+++ SnakeLadder/Program.cs
@@ -1,30 +1,41 @@
 ﻿internal class Program
 {
     private static void Main(string[] args)
-    {
+    {  
+            int playerPosition = 0;
+            int diceRolls = 0;
+            Random random = new Random();
+            while (playerPosition != 100)
+            {
+                int diceNumber = random.Next(1, 7);
+                diceRolls++;
+                Console.WriteLine("Dice rolled: " + diceNumber);
+                int option = random.Next(0, 3);
+                switch (option)
+                {
+                    case 0:
+                        Console.WriteLine("No Play. Stay in the same position.");
+                        break;
+                    case 1:
+                        Console.WriteLine("Ladder! Move ahead by " + diceNumber + " positions.");
+                        playerPosition += diceNumber;
+                        break;
+                    case 2:
+                        Console.WriteLine("Snake! Move back by " + diceNumber + " positions.");
+                        playerPosition -= diceNumber;
+                        break;
+                }
+                if (playerPosition < 0)
+                {
+                    playerPosition = 0;
+                }
+                if (playerPosition > 100)
+                {
+                    playerPosition -= diceNumber;
+                }
 
-        int playerPosition = 0;
-        int diceRolls = 0;
-        Random random = new Random();
-        while (playerPosition != 100)
-        {
-            int diceNumber = random.Next(1, 7);
-            diceRolls++;
-            Console.WriteLine("Dice rolled: " + diceNumber);
-
-            int option = random.Next(0, 3);
-
-
-            switch (option)
-            { }
-            playerPosition -= diceNumber;
-
-            break;
-
-        }
-        if (playerPosition < 0)
-        {
-            playerPosition = 0;
-        }
+                Console.WriteLine("Current position: " + playerPosition);
+                Console.WriteLine();
+            }
     }
 }
